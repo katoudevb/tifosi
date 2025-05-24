dc9f2b15e5bf135b77486a74c65652e3b8a699d6
katoudevb <katoudevb@gmail.com>
2025-05-24 07:19:51 +0200

    Add files via upload

---
+++ bdd_tifosi.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 22 mai 2025 à 14:15
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tifosi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Déchargement des données de la table `boisson`
+--
+
+INSERT INTO `boisson` (`id_boisson`, `nom_boisson`) VALUES
+(1, 'Coca-cola zéro'),
+(2, 'Coca-cola original'),
+(3, 'Fanta citron'),
+(4, 'Fanta orange'),
+(5, 'Capri-sun'),
+(6, 'Pepsi'),
+(7, 'Pepsi Max Zéro'),
+(8, 'Lipton zéro citron'),
+(9, 'Lipton Peach'),
+(10, 'Monster energy ultra gold'),
+(11, 'Monster energy ultra blue'),
+(12, 'Eau de source ');
+
+-- --------------------------------------------------------
+
+--
+-- Déchargement des données de la table `focaccia`
+--
+
+INSERT INTO `focaccia` (`id_focaccia`, `nom_focaccia`, `prix_focaccia`) VALUES
+(1, 'Mozaccia', 9.8),
+(2, 'Gorgonzollaccia', 10.8),
+(3, 'Raclaccia', 8.9),
+(4, 'Emmentalaccia', 9.8),
+(5, 'Tradizione', 8.9),
+(6, 'Hawaienne', 11.2),
+(7, 'Américaine', 10.8),
+(8, 'Paysanne', 12.8);
+
+-- --------------------------------------------------------
+
+--
+-- Déchargement des données de la table `ingrédient`
+--
+
+INSERT INTO `ingrédient` (`id_ingrédient`, `nom_ingrédient`) VALUES
+(1, 'Ail'),
+(2, 'Ananas'),
+(3, 'Artichaut'),
+(4, 'Bacon'),
+(5, 'Base Tomate'),
+(6, 'Base crème'),
+(7, 'Champignon'),
+(8, 'Chevre'),
+(9, 'Cresson'),
+(10, 'Emmental'),
+(11, 'Gorgonzola'),
+(12, 'Jambon cuit'),
+(13, 'Jambon fumé'),
+(14, 'Oeuf'),
+(15, 'Oignon'),
+(16, 'Olive noire'),
+(17, 'Olive verte'),
+(18, 'Parmesan'),
+(19, 'Piment'),
+(20, 'Poivre'),
+(21, 'Pomme de terre'),
+(22, 'Raclette'),
+(23, 'Salami'),
+(24, 'Tomate cerise');
+
+-- --------------------------------------------------------
+
+--
+-- Déchargement des données de la table `marque`
+--
+
+INSERT INTO `marque` (`id_marque`, `nom_marque`) VALUES
+(1, 'Coca-cola'),
+(2, 'Cristalline'),
+(3, 'Monster'),
+(4, 'Pepsico');
+
+-- --------------------------------------------------------
