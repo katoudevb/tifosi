8894fb8e019ca0e22a1d8edda106588937806b53
katoudevb <katoudevb@gmail.com>
2025-05-24 07:28:28 +0200

    Add files via upload

---
+++ data_test.sql
@@ -0,0 +1,278 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 22 mai 2025 à 14:15
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tifosi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `achete`
+--
+
+CREATE TABLE `achete` (
+  `jour` date NOT NULL,
+  `id_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `appartient`
+--
+
+CREATE TABLE `appartient` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boisson`
+--
+
+CREATE TABLE `boisson` (
+  `id_boisson` int(11) NOT NULL,
+  `nom_boisson` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `nom_client` varchar(45) NOT NULL,
+  `age` int(11) NOT NULL,
+  `cp_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comprend`
+--
+
+CREATE TABLE `comprend` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contient`
+--
+
+CREATE TABLE `contient` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `estconstitue`
+--
+
+CREATE TABLE `estconstitue` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `focaccia`
+--
+
+CREATE TABLE `focaccia` (
+  `id_focaccia` int(11) NOT NULL,
+  `nom_focaccia` varchar(45) NOT NULL,
+  `prix_focaccia` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingrédient`
+--
+
+CREATE TABLE `ingrédient` (
+  `id_ingrédient` int(11) NOT NULL,
+  `nom_ingrédient` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marque`
+--
+
+CREATE TABLE `marque` (
+  `id_marque` int(11) NOT NULL,
+  `nom_marque` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `nom_menu` varchar(45) NOT NULL,
+  `prix_menu` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paye`
+--
+
+CREATE TABLE `paye` (
+  `jour` date NOT NULL,
+  `id_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `achete`
+--
+ALTER TABLE `achete`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `appartient`
+--
+ALTER TABLE `appartient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  ADD PRIMARY KEY (`id_boisson`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `comprend`
+--
+ALTER TABLE `comprend`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `estconstitue`
+--
+ALTER TABLE `estconstitue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `focaccia`
+--
+ALTER TABLE `focaccia`
+  ADD PRIMARY KEY (`id_focaccia`);
+
+--
+-- Index pour la table `ingrédient`
+--
+ALTER TABLE `ingrédient`
+  ADD PRIMARY KEY (`id_ingrédient`);
+
+--
+-- Index pour la table `marque`
+--
+ALTER TABLE `marque`
+  ADD PRIMARY KEY (`id_marque`);
+
+--
+-- Index pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Index pour la table `paye`
+--
+ALTER TABLE `paye`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `achete`
+--
+ALTER TABLE `achete`
+  ADD CONSTRAINT `achete_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `appartient`
+--
+ALTER TABLE `appartient`
+  ADD CONSTRAINT `appartient_ibfk_1` FOREIGN KEY (`id`) REFERENCES `marque` (`id_marque`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `comprend`
+--
+ALTER TABLE `comprend`
+  ADD CONSTRAINT `comprend_ibfk_1` FOREIGN KEY (`id`) REFERENCES `focaccia` (`id_focaccia`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD CONSTRAINT `contient_ibfk_1` FOREIGN KEY (`id`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `estconstitue`
+--
+ALTER TABLE `estconstitue`
+  ADD CONSTRAINT `estconstitue_ibfk_1` FOREIGN KEY (`id`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `paye` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `paye`
+--
+ALTER TABLE `paye`
+  ADD CONSTRAINT `paye_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
